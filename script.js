68c86f687275532e5724404eaa9493b81b9471fe
lucasxnarezzi <lucasxnarezzi@gmail.com>
2024-06-11 18:24:13 -0300

    anexando projeto de relógio digital ao github

---
+++ script.js
@@ -0,0 +1,18 @@
+const horas = document.getElementById('horas');
+const minutos = document.getElementById('minutos');
+const segundos = document.getElementById('segundos');
+
+const relogio = setInterval(function time() {
+    let dateToday = new Date()
+    let hour = dateToday.getHours();
+    let minute = dateToday.getMinutes();
+    let second = dateToday.getSeconds();
+
+    if (hour < 10) hour = '0' + hour;
+    if (minute < 10) hour = '0' + minute;
+    if (second < 10) hour = '0' + second;
+
+    horas.textContent = hour;
+    minutos.textContent = minute;
+    segundos.textContent = second;
+})
